8fa34d05f6f7f9672bb86bb43df6ce5764290465
FlashCodx <47036505+FlashCodX@users.noreply.github.com>
2020-09-01 23:40:54 +0100

    Updates

---
+++ precache-manifest.203335379105bd90d33ae3c7ebde8a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53160a66a42622be85943f52c3b10f3",
+    "url": "/Spotify/index.html"
+  },
+  {
+    "revision": "16e43510665e15175746",
+    "url": "/Spotify/static/css/main.d37d088e.chunk.css"
+  },
+  {
+    "revision": "e253f58b2e763487731a",
+    "url": "/Spotify/static/js/2.98414ce2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Spotify/static/js/2.98414ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e43510665e15175746",
+    "url": "/Spotify/static/js/main.4712737a.chunk.js"
+  },
+  {
+    "revision": "0bb7c02e93bc452f0f2d",
+    "url": "/Spotify/static/js/runtime-main.cf721651.js"
+  }
+]);
